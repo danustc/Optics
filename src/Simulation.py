86260ef4440d1cd4c2f403dd97ea6126213d0ff8
danustc <xdlaway@gmail.com>
2016-07-25 13:16:49 -0700

    0725_Rock1

--- src/Simulation.py
+++ src/Simulation.py
@@ -1,14 +1,27 @@
 """
- Created by Dan Xie, 07/15/2016
- A systematic simulation of o
-
+ Last update by Dan Xie, 07/25/2016
+ A systematic simulation of optical systems, based on POPPY package.
 
 
 """
 
 
-
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 import poppy 
+import logging
+from poppy import poppy_core
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+osys  = poppy.OpticalSystem()
+osys.add_pupil(poppy.CircularAperture(radius=3))    # pupil radius in meters
+osys.add_detector(pixelscale=0.010, fov_arcsec=5.0)  # image plane coordinates in arcseconds
+
+psf = osys.calc_psf(2e-6)                            # wavelength in microns
+poppy.display_PSF(psf, title='The Airy Function')
+
 
