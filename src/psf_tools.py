633a32365a33cfffddfe08cb8455abf08c835773
danustc <xdlaway@gmail.com>
2016-08-11 22:45:15 -0700

    0811_home-2

--- src/psf_tools.py
+++ src/psf_tools.py
@@ -54,9 +54,31 @@ def psf_recenter(stack, r_mask = 40, cy_ext = 1.5):
             # Background estimation
 
 
+def psf_slice(stack, dim_slice = 0, n_slice = None):
+    """
+    take out a slice from a stack and return it. 
+    """
+    if n_slice is None:
+        n_slice = stack.shape[dim_slice]/2 # take from the middle
+    
+    if (dim_slice == 0):
+        # take an xy slice on
+        pslice = stack[n_slice]
+    elif(dim_slice == 1):
+        # take an xz slice
+        pslice = stack[:, n_slice, :]
+    else: 
+        # take an yz slice 
+        pslice = stack[:,:,n_slice]
+    
+    return pslice
+    # end of psf_slice
+
+
 def psf_zplane(stack, dz, w0, de = 1):
     '''
     determine the position of the real focal plane.
+    Don't mistake with psf_slice! 
     '''
     nz, ny, nx = stack.shape 
     cy, cx = np.unravel_index(np.argmax(gf(stack,2)), (nz,ny,nx))[1:]
@@ -119,7 +141,7 @@ def psf_lineplot(stack, cut_range = 2, axis = 0, z_step = 0.3, r_step=0.097, c_p
     # done with psf_lineplot
     
 
-def psf_planeplot(stack, plane = 0, px_radius = 50, c_pxl = None, argmt = None):
+def psf_planeplot(stack, plane = 0, c_pxl = None, argmt = None):
     """
     select one or more planes to display
     argmt: arrangement of multiple plots 
@@ -129,16 +151,52 @@ def psf_planeplot(stack, plane = 0, px_radius = 50, c_pxl = None, argmt = None):
     cz, cy, cx = np.unravel_index(np.argmax(stack), (nz,ny,nx))
     centers = [cz, cy, cx]
     
+   
+    
     if c_pxl is None: 
         # plot where the maximum is 
         c_pxl = centers[plane]
     
     if(np.isscalar(c_pxl) == True):
         # If we only plot one frame
-        figp = plt.figure(figsize = (side_0,side_0))
+        pslice = psf_slice(stack, plane, c_pxl)
+        py, px = pslice.shape
+        figp = plt.figure(figsize = (side_0,side_0*py/px))
+        ax = figp.add_subplot(1,1,1)
+        ax.imshow(pslice, cmap = 'Grey_r', interpolation = 'none')
+        ax.tick_params(
+            axis = 'both',
+            which = 'both',
+            bottom = 'off',
+            top = 'off',
+            right = 'off',
+            left = 'off',
+            labelleft='off',
+            labelbottom = 'off')
+        # All the units are in pixels, no microns involved
     else:
         # plot multiple frames in one figure
-        figp = plt.figure(figsize = (side_0, side_0*argmt[1]/argmt[0]))
+        # if the length of c_pxl is smaller than the arrangement, then stop at c_pxl;
+        # otherwise stop when the arrangement is full.
+        n_stop = np.min(len(c_pxl), np.prod(argmt)) 
+        ii = 1
+        figp = plt.figure(figsize = (side_0, side_0*argmt[0]/argmt[1])) # scale 
+        for n_slice in c_pxl[:n_stop]:
+            # plot one by one 
+            pslice = psf_slice(stack, plane, n_slice)
+            ax = figp.add_subplot(argmt[0], argmt[1], ii)
+            ax.imshow(pslice, cmap = 'Grey_r', interpolation = 'none')
+            ax.tick_params(
+                axis = 'both',
+                which = 'both',
+                bottom = 'off',
+                top = 'off',
+                right = 'off',
+                left = 'off',
+                labelleft='off',
+                labelbottom = 'off')
+            
 
+    plt.tight_layout()
     return figp
     # end of psf_planeplot
