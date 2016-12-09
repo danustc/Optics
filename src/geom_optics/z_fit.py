e6f1879240f22ec9657c9945117d35994ddf2434
danustc <xdlaway@gmail.com>
2016-12-08 22:37:41 -0800

    1208_Home-2

---
+++ src/geom_optics/z_fit.py
@@ -0,0 +1,11 @@
+'''
+Created by Dan on 12/08/2016. For zernike fits.
+'''
+import numpy as np
+import libtim.zern as zern
+
+
+
+
+def 
+fitResults = libtim.zern.fit_zernike(unwrapped, rad=radius, nmodes=2    5)
